6a226ae66d4180d5b5500db43431411411db9a15
Snow Pettersen <kpettersen@netflix.com>
2020-03-03 05:14:01 -0800

    make it work with proxyd repo

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,7 @@
 . ci/setup_cache.sh
 trap - EXIT
 
-BAZELRC_FILE=~/.bazelrc bazel/setup_clang.sh /opt/llvm
+BAZELRC_FILE=~/.bazelrc envoy/bazel/setup_clang.sh /opt/llvm
 
 # Use generated toolchain config because we know the base container is the one we're using in RBE.
 # Not using libc++ here because clangd will raise some tidy issue in libc++ header as of version 9.
